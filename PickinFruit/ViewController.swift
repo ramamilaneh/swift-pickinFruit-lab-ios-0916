9ab770aa58b40c945e4bf2f491e7ea8f25310a9d
Rama Milaneh <ramamilaneh@Ramas-MacBook-Air.local>
2016-10-20 15:17:01 -0400

    Done

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+    
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
     
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
@@ -19,15 +21,59 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
+        self.pickerView.delegate = self
         
     }
     
-    @IBAction func spinButtonTapped(_ sender: UIButton) {
-        
-        
-        
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
     }
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+       // return self.fruitsArray.count
+        return 400
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let index = row % fruitsArray.count
+        return self.fruitsArray[index]
+    }
+    
+    
+    @IBAction func spinButtonTapped(_ sender: UIButton) {
+        self.resultLabel.alpha = 1
+        var resultArray = [Int]()
+        for component in 0..<pickerView.numberOfComponents {
+            let row = Int(arc4random_uniform(UInt32(fruitsArray.count)))
+            pickerView.selectRow(row, inComponent: component, animated: true)
+            resultArray.append(pickerView.selectedRow(inComponent: component))
+            
+        }
+        if resultArray[0] == resultArray[1] && resultArray[1] == resultArray[2] {
+            
+            self.resultLabel.text = "WINNER!"
+        }
+        else{
+            self.resultLabel.text = "TRY AGAIN"
+            
+        }
+      animateTheLabel()
+    }
+    
+    func animateTheLabel() {
+        UIView.animateKeyframes(withDuration: 0.8, delay: 0.0, options: [.allowUserInteraction], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/2, animations: {
+                self.resultLabel.alpha = 0
+            })
+            UIView.addKeyframe(withRelativeStartTime: 1/2, relativeDuration: 1/2, animations: {
+                self.resultLabel.alpha = 1
+            })
+            }) { (success) in
+                self.resultLabel.alpha = 0
+        }
+        
+            }
+    
 }
 
 // MARK: Set Up
